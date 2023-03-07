d4d3dad7e27b4845a5b76ec25ea85588e00dd474
serlaic <s.laicuks@wesec.co.uk>
2023-03-07 22:19:34 +0000

    adds method which will take a table name as argument and return pandas DataFrame in DataExtractor class

--- data_extraction.py
+++ data_extraction.py
@@ -1,2 +1,31 @@
 class DataExtractor():
-    pass
+    
+
+    def __init__(self, table_name, db_conn):
+        self.table_name = table_name
+        self.db_conn = db_conn
+
+    def read_rds_table(self):
+        '''
+        This function will take a table name as an argument and 
+        return a pandas DataFrame.
+        '''
+        import pandas as pd
+        return pd.read_sql_table(self.table_name, self.db_conn.init_db_engine())
+    
+
+
+
+
+
+
+
+        
+
+        
+
+
+
+
+
+        
