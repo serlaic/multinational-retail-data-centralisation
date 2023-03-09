ce5568414e8b3594bcca4c4bd6fc28bb807b95d6
serlaic <s.laicuks@wesec.co.uk>
2023-03-09 16:31:45 +0000

    adds extract_from_s3 method which will extract files from the s3 server

--- data_extraction.py
+++ data_extraction.py
@@ -15,7 +15,7 @@ class DataExtractor(object):
         This function will take a pdf file and return pandas dataframe
         '''
         import tabula
-        pdf_table_df = tabula.read_pdf('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf', pages='all', multiple_tables = False)
+        pdf_table_df = tabula.read_pdf('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf', pages = 'all', multiple_tables = False)
         return pdf_table_df[0]
 
     @classmethod
@@ -27,10 +27,8 @@ class DataExtractor(object):
         '''
         import requests
         api_key = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
-        # requests information
-        response = requests.get('https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores', headers= api_key)
-        # converts response into dictionary
-        store_number = response.json()
+        # requests information and converts to json
+        store_number = requests.get('https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores', headers= api_key).json()
         # returns the number of stores
         return store_number['number_stores']
 
@@ -45,19 +43,39 @@ class DataExtractor(object):
         api_key = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
         store_data_df = pd.DataFrame()
         # for loop to go through all the pages and add them into the store_data_df dataframe
-        for store_number in range(0,DataExtractor().list_number_of_stores()):
-            # requests information
-            response = requests.get(f"https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}", headers= api_key)
-            # converts response into dictionary
-            store_data_json = response.json()
+        for store_number in range(0, DataExtractor.list_number_of_stores()):
+            # requests information and converts to json
+            store_data_json = requests.get(f"https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}", headers= api_key).json()
             # converts dictionary into dataframe
             store_data_df_single = pd.DataFrame.from_dict([store_data_json])
             # adds single dataframe into final dataframe after each iterration
-            store_data_df = pd.concat([store_data_df, store_data_df_single],ignore_index= True)
+            store_data_df = pd.concat([store_data_df, store_data_df_single],ignore_index = True)
         # returns final dataframe with all the data
         return store_data_df
+    
+    @classmethod
+    def extract_from_s3(cls, s3_address):
+        '''
+        This method will use boto3 package to download and extract
+        the information returning pandas Dataframe.
+        '''
+        import boto3
+        import pandas as pd
+        import urllib
 
-        
+        link_segments = s3_address.split('/')
+        link_segments[0] = link_segments[0].replace(':','')
+        link_segments.remove('')
+        client = link_segments[0]
+        folder = link_segments[-2]
+        file = link_segments[-1]    
+      #  s3 = boto3.client(client)
+      #  s3.download_file(folder, file, file)
+        products_csv = pd.read_csv(file)
+        return print(products_csv)
+      
+
+      
 
         
 
