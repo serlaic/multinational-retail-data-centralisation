1690bdd425b68eded0849d2718f3cbcaac42fa87
serlaic <s.laicuks@wesec.co.uk>
2023-03-10 01:53:43 +0000

    adds extract_json_from_s3 method in DataExtractor class which will open json file from s3  weblink and convert to dataframe

--- data_extraction.py
+++ data_extraction.py
@@ -7,7 +7,8 @@ class DataExtractor(object):
         return a pandas DataFrame.
         '''
         import pandas as pd
-        return pd.read_sql_table(table_name, db_conn.init_db_engine())
+        dataframe = pd.read_sql_table(table_name, db_conn.init_db_engine())
+        return dataframe
     
     @classmethod
     def retrieve_pdf_data(cls):
@@ -27,9 +28,10 @@ class DataExtractor(object):
         '''
         import requests
         api_key = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
+
         # requests information and converts to json
         store_number = requests.get('https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores', headers= api_key).json()
-        # returns the number of stores
+
         return store_number['number_stores']
 
     @classmethod
@@ -42,38 +44,63 @@ class DataExtractor(object):
         import pandas as pd
         api_key = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
         store_data_df = pd.DataFrame()
-        # for loop to go through all the pages and add them into the store_data_df dataframe
+
+        # for loop to iterate through all the pages and add them into the store_data_df dataframe
         for store_number in range(0, DataExtractor.list_number_of_stores()):
-            # requests information and converts to json
             store_data_json = requests.get(f"https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}", headers= api_key).json()
-            # converts dictionary into dataframe
             store_data_df_single = pd.DataFrame.from_dict([store_data_json])
-            # adds single dataframe into final dataframe after each iterration
             store_data_df = pd.concat([store_data_df, store_data_df_single],ignore_index = True)
-        # returns final dataframe with all the data
+        
         return store_data_df
     
     @classmethod
-    def extract_from_s3(cls, s3_address):
+    def extract_from_s3(cls):
         '''
         This method will use boto3 package to download and extract
         the information returning pandas Dataframe.
         '''
         import boto3
         import pandas as pd
-        import urllib
 
+        s3_address = "s3://data-handling-public/products.csv"
         link_segments = s3_address.split('/')
         link_segments[0] = link_segments[0].replace(':','')
         link_segments.remove('')
         client = link_segments[0]
         folder = link_segments[-2]
-        file = link_segments[-1]    
-      #  s3 = boto3.client(client)
-      #  s3.download_file(folder, file, file)
-        products_csv = pd.read_csv(file)
-        return print(products_csv)
+        file = link_segments[-1]
+
+        # Reads the file if it exists and runs boto3 client 
+        # and tries to download the file if doesn't exist.
+        try:    
+            products_csv = pd.read_csv(file)
+        except:
+            print('File doesn\'t exist. Trying to download the file...')
+            try:
+                s3 = boto3.client(client)
+                s3.download_file(folder, file, file)
+            except:
+                print('Check link. Should look like s3://.../(filename)')
+
+        return products_csv
       
+    @classmethod
+    def extract_json_from_s3(cls):
+        '''
+        This method will download json file from url and convert into dataframe.
+        '''
+        import requests
+        import pandas as pd
+
+        # Change this with your URL
+        s3_address = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/date_details.json'
+
+        json_file = requests.get(s3_address).json()
+        datetime_df = pd.DataFrame.from_dict(json_file).dropna()
+
+        return datetime_df
+    
+
 
       
 
